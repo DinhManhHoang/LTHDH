af10252da1d2ea26cb63cb201d3f6ebd1ea1c214
DinhManhHoang <dinhmanhhoang150197@gmail.com>
2018-05-07 10:13:25 +0700

    2nd

---
+++ RootLayoutController.java
@@ -0,0 +1,15 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class RootLayoutController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}
